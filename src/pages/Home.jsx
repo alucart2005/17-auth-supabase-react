530d636fe5d19947098f27f0aef48ed5a2d051ae
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-04-06 15:29:42 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import googlelogo from "../assets/logogoogle.png";
+import { Perfil } from "../components/Perfil";
+
+export function Home() {
+  return ( <div className="App">
+   <Perfil foto="foto" name="name" email="email"/>
+  <h1>SUPABASE es COOL</h1>
+  <img src={googlelogo} className="logo google" alt="React logo" />
+  <div className="card">
+    <button>Logout</button>
+    <p>codewizardai.com</p>
+  </div>
+  <p className="read-the-docs">
+  Supabase implements all the power of PostgreSQL
+  </p>
+</div>);
+}
